6ac3a66ec1af3292a864eac6aec0922f741136a4
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-08-20 22:26:39 +0900

    Firebase auth (#3)
    * add TypeScriptの設定ファイルを追加
    
    * add .gitignoreにfirebaseのAPIキーを追加
    
    * fix APIキーの設定を修正
    
    * add firebaseのログインを実装
    
    * fix firebaseを削除
    
    * update login関係を実装
    
    非ログイン時強制的にログインページへ、ログイン時にindex.htmlにPOST接続

---
+++ dist/script/modules.js
@@ -0,0 +1,20 @@
+// 無理やりPOSTするための関数
+export function post(path, params, method) {
+    if (method === void 0) { method = 'post'; }
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement('input');
+            hiddenField.type = 'hidden';
+            hiddenField.name = key;
+            hiddenField.value = params[key];
+            form.appendChild(hiddenField);
+        }
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
---
+++ src/firebase.ts
@@ -0,0 +1,40 @@
+import { firebaseConfig } from './firebase_config.js'
+import { post } from './modules.js'
+
+firebase.initializeApp(firebaseConfig);
+
+const uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        return true;
+      },
+      uiShown: function() {
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    signInFlow: 'popup',
+    signInSuccessUrl: 'temp.html',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+};
+
+if (location.pathname === '/Hack_U_2021_Vol1/dist/login.html') {
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start('#firebaseui-auth-container', uiConfig);
+}
+// 非ログイン時、強制的にログインページへ
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!user && location.pathname !== '/Hack_U_2021_Vol1/dist/login.html') {
+      window.location.href = '/Hack_U_2021_Vol1/dist/login.html';
+  }
+});
+// ログイン後に直接「/home/<user_id>」に飛べないので代替処置
+// 我ながら頭悪いと思う
+if (location.pathname === '/Hack_U_2021_Vol1/dist/temp.html') {
+  firebase.auth().onAuthStateChanged(function (user) {
+    const data = {"userid": user.uid, "username": user.displayName}
+    post('/Hack_U_2021_Vol1/dist/index.html', data)
+  });
+}
