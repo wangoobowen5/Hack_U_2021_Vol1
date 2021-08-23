5f68d1dc458f9eac6a3942a6426f62508cab7af6
yochimonji <yochimonji@gmail.com>
2021-08-24 00:07:06 +0900

    update PC版のHTML/CSS完成

--- dist/user.php
+++ dist/user.php
@@ -26,30 +26,30 @@
 
     <main>
         <div class="main-rapper common-rapper">
-            <div class="main-content flex">
+            <div class="main-content flex user-area">
                 <div class="main-user flex">
                     <div class="btn-circle-border-simple"></div>
-                    <h2 class="user-name">ユーザー名</h2>
+                    <h1 class="user-name">ユーザー名</h1>
                 </div>
                 <p><button class="logout-button common-button">ログアウト</button></p>
             </div>
             <div class="main-content">
                 <div class="schedule-area flex">
                     <div class="schedule-description">
-                        <h3 class="schedule-title">予定を追加しても良い時間</h3>
-                        <p class="schedule-text">入力した時間に自動で予定を追加します。</p>
+                        <h2 class="schedule-title">予定を追加しても良い時間</h2>
+                        <p class="schedule-text">入力した時間内に自動で予定を追加します。</p>
                     </div>
-                    <p><button class="schedule-update-button">更新</button></p>
+                    <p><button class="schedule-update-button common-button">更新</button></p>
                 </div>
                 <div class="schedule-time flex">
-                    <div class="begin">
-                        <h3 class="begin-text">始まり</h3>
-                        <div class="begin-input"></div>
+                    <div class="begin flex">
+                        <h2 class="begin-text">何時から</h2>
+                        <div class="begin-input time-input"></div>
                     </div>
-                    <h3>〜</h3>
-                    <div class="end">
-                        <h3 class="end-text">始まり</h3>
-                        <div class="end-input"></div>
+                    <h2>〜</h2>
+                    <div class="end flex">
+                        <h2 class="end-text">何時まで</h2>
+                        <div class="end-input time-input"></div>
                     </div>
                 </div>
             </div>
