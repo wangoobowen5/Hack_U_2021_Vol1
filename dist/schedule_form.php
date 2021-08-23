9eba414dc366994bb9b0cb3de3fd0e115e6a38e3
shrbt6 <yutaro.nagoya@gmail.com>
2021-08-24 02:32:18 +0900

    fix パスの修正

--- dist/schedule_form.php
+++ dist/schedule_form.php
@@ -13,9 +13,9 @@
 		</div>
 
         <div class="front">
-            <a href="play.html" class="home">ホーム</a>
-            <a href="play.html" class="calendar">カレンダー</a>
-            <a href="play.html" class="btn-circle-border-simple"></a>
+            <a href="./index.php" class="home">ホーム</a>
+            <a href="./template.php" class="calendar">テンプレート</a>
+            <a href="./user.php" class="btn-circle-border-simple"></a>
         </div>
         <hr color="black" class="line">
 
