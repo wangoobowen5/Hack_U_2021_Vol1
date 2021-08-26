cf8fe5857059a9b9eade2607bc16a43080ec6140
yochimonji <yochimonji@gmail.com>
2021-08-26 23:02:46 +0900

    update indexからget_planへuseridを送れるように更新

--- dist/get_plan.php
+++ dist/get_plan.php
@@ -1,12 +1,31 @@
 <?php
-  // 画面から送られたきた値
-  $title = "test";	// $_POST['id']とも書ける
-  $color = "#257e4a";
-  $start = $_GET['start'];
-  $end = $_GET['end'];
+  // index.jsからuserid, start, stopを取得
+  $userid = $_GET["userid"];
+  $start = $_GET["start"];
+  $end = $_GET["end"];
 
-
-  $list = array(array("title" => $title, "start" => $start, "end" => $end, "color" => $color));
+  // 本来はデータベースからlistに代入
+  $list = array(
+    array(
+        "title" => "何でも相談会",
+        "start" => "2021-08-19T13:00:00",
+        "end" => "2021-08-19T17:00:00",
+        "color" => "#EDAD0B",
+        "userid" => $userid
+    ),
+    array(
+        "title" => "ミーティング",
+        "start" => "2021-08-24T10:00:00",
+        "end" => "2021-08-24T11:00:00",
+        "color" => "#257e4a"
+    ),
+    array(
+        "title" => "HackU",
+        "start" => "2021-08-28",
+        "end" => "2021-08-28",
+        "color" => "#99CFE5"
+    )
+  );
   // 明示的に指定しない場合は、text/html型と判断される
   header("Content-type: application/json; charset=UTF-8");
   //JSONデータを出力
--- dist/script/index.js
+++ dist/script/index.js
@@ -1,54 +1,59 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay, listWeek'
-        },
-        locale: 'ja',
-        eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
-        editable: true,
-        events: {
-            url: './get_plan.php',
-            method: 'POST',
-            failure: function () {
-                console.error('there was an error while fetching events!');
+    firebase.auth().onAuthStateChanged(function (user) {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay, listWeek'
+            },
+            locale: 'ja',
+            eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
+            editable: true,
+            // events: './get_plan.php',
+            events: {
+                url: './get_plan.php',
+                extraParams: {
+                    userid: user.uid
+                }
+            },
+            // events: [  // バックエンドが出来次第削除
+            // {
+            //     id: '1111',
+            //     title: '何でも相談会',
+            //     start: '2021-08-19T13:00:00',
+            //     end: '2021-08-19T17:00:00',
+            //     color: '#EDAD0B'
+            // },
+            // {
+            //     title: 'ミーティング',
+            //     start: '2021-08-24T10:00:00',
+            //     end: '2021-08-24T11:00:00',
+            //     color: '#257e4a'
+            // },
+            // {
+            //     title: 'HackU',
+            //     start: '2021-08-28',
+            //     end: '2021-08-28',
+            //     color: '#99CFE5'
+            // }
+            // ],
+            dateClick: function (info) {
+                location.href = './schedule_form.php?date=' + info.dateStr; // schedule_formが出来次第変更
+            },
+            eventClick: function (info) {
+                console.log('Event: ' + info.event.title);
             }
-        },
-        // events: [  // バックエンドが出来次第削除
-        // {
-        //     id: '1111',
-        //     title: '何でも相談会',
-        //     start: '2021-08-19T13:00:00',
-        //     end: '2021-08-19T17:00:00',
-        //     color: '#EDAD0B'
-        // },
-        // {
-        //     title: 'ミーティング',
-        //     start: '2021-08-24T10:00:00',
-        //     end: '2021-08-24T11:00:00',
-        //     color: '#257e4a'
-        // },
-        // {
-        //     title: 'HackU',
-        //     start: '2021-08-28',
-        //     end: '2021-08-28',
-        //     color: '#99CFE5'
-        // }
-        // ],
-        dateClick: function (info) {
-            location.href = './schedule_form.php?date=' + info.dateStr; // schedule_formが出来次第変更
-        },
-        eventClick: function (info) {
-            console.log('Event: ' + info.event.title);
-        }
+        });
+        calendar.render();
     });
-    calendar.render();
+    // モーダル表示用コード
     $(".info").modaal({
         overlay_close: true,
+        width: 800,
+        height: 800,
         before_open: function () {
             $('html').css('overflow-y', 'hidden'); /*縦スクロールバーを出さない*/
         },
