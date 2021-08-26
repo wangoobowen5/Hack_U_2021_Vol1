6db5ca9c839535f05428cf75c8b31e42caae7ad0
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-08-26 16:28:20 +0900

    Fix path (#20)
    * fix path
    
    * remove add_plan.phpを削除

--- dist/script/schedule_form.js
+++ dist/script/schedule_form.js
@@ -6,18 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
         var scheduleName = document.getElementById('schedule-name').value;
         var templateList = document.getElementById("template-list");
         var num = templateList.selectedIndex;
-        var templateName = templateList.options[num].value;
+        var templateId = templateList.options[num].value;
         var startDate = document.getElementById('start-date').value;
         var endDate = document.getElementById('end-date').value;
         firebase.auth().onAuthStateChanged(function (user) {
             var data = {
                 "userid": user.uid,
-                "scheduleName": scheduleName,
-                "templateName": templateName,
-                "start-date": startDate,
-                "end-date": endDate
+                "title": scheduleName,
+                "templateid": templateId,
+                "start_date": startDate,
+                "end_date": endDate
             };
-            post('/Hack_U_2021_Vol1/dist/index.php', data);
+            post('/Hack_U_2021_Vol1/dist/add_plan.php', data);
         });
     }, false);
 }, false);
--- src/schedule_form.ts
+++ src/schedule_form.ts
@@ -8,18 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const scheduleName: string = document.getElementById('schedule-name').value;
         const templateList: any = document.getElementById("template-list");
         const num: number = templateList.selectedIndex;
-        const templateName: string = templateList.options[num].value;
+        const templateId: string = templateList.options[num].value;
         const startDate: string = document.getElementById('start-date').value;
         const endDate: string = document.getElementById('end-date').value;
         firebase.auth().onAuthStateChanged(function (user) {
             const data: any = {
                 "userid": user.uid,
-                "scheduleName": scheduleName,
-                "templateName": templateName,
-                "start-date": startDate,
-                "end-date": endDate
+                "title": scheduleName,
+                "templateid": templateId,
+                "start_date": startDate,
+                "end_date": endDate
             };
-            post('/Hack_U_2021_Vol1/dist/index.php', data);
+            post('/Hack_U_2021_Vol1/dist/add_plan.php', data);
         });
     }, false);
 }, false);
