818468444f55a424c8695f238de860eaf0675d1b
wangoobowen5 <wangoobowen5@gmail.com>
2021-08-27 22:39:38 +0900

    アップデートしました！

--- dist/add_plan.php
+++ dist/add_plan.php
@@ -2,9 +2,19 @@
 // useridの追加
 $userid = $_POST["userid"];
 $title = $_POST["title"];
-$templateid = $_POST["templateid"];
+$templateid = (int)$_POST["templateid"];
 $start_date = $_POST["start_date"];
 $end_date = $_POST["end_date"];
+
+if($templateid == 1){
+    $progress = "内容書き出し:0,スライド:0,発表練習:0";
+}
+elseif($templateid == 2){
+    $progress = "調査:20,レポートを書く:80";
+}
+elseif($templateid == 3){
+    $progress = "テキストの復習:50,練習問題:50";
+}
 
 try {
     $dsn = 'mysql:dbname=bislab_db;host=localhost';
@@ -26,13 +36,14 @@ try {
                 $stmt->execute($data);
             }
 
-            $sql2 = "INSERT INTO templatetbl (userid,title,templateid,start_date,end_date) values (?,?,?,?,?)";
+            $sql2 = "INSERT INTO scheduletbl (userid,title,templateid,start_date,end_date,progress) values (?,?,?,?,?,?)";
             $stmt = $dbh->prepare($sql2);
             $data[] = $userid;
             $data[] = $title;
             $data[] = $templateid;
             $data[] = $start_date;
             $data[] = $end_date;
+            $data[] = $progress;
             $stmt->execute($data);
         
 
@@ -43,5 +54,7 @@ catch(Exception $e){
     print 'サーバが停止しておりますので暫くお待ちください。';
     exit();
 }
+
+header("Location: /Hack_U_2021_Vol1/dist/index.php"); 
 ?>
 
